e3cebb8fbbb2eebda7264f00640c8307230be7bb
דניאל פרידקין <57833477+danielfree19@users.noreply.github.com>
2021-05-15 14:08:56 +0300

    four

--- class/ongoing/Jobapp.java
+++ class/ongoing/Jobapp.java
@@ -3,5 +3,20 @@ package ongoing;
 public class Jobapp {
     private int jobID;
     private String CV;
-    
+    public Jobapp(int jobID, String CV) {
+        this.jobID = jobID;
+        this.CV = CV;
+    }
+    int getJobID() {
+        return this.jobID;
+    }
+    String getCV() {
+        return this.CV;
+    }
+    void setJobID(int jobID) {
+        this.jobID = jobID;
+    }
+    void setCV(String CV) {
+        this.CV = CV;
+    }
 }
