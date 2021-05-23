ea0ebb97bba4863ceaf407a9f6c38ad958243918
דניאל פרידקין <57833477+danielfree19@users.noreply.github.com>
2021-05-23 19:10:41 +0300

    minor fixes

--- class/windows/EmployeeWindow.java
+++ class/windows/EmployeeWindow.java
@@ -1,4 +1,5 @@
 package windows;
+
 import Server.myJDBC;
 import ongoing.List;
 import ongoing.Person;
@@ -8,7 +9,6 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -19,12 +19,12 @@ public class EmployeeWindow {
     private JPanel panel;
     private JTable table;
     private JScrollPane scrollPane;
-    private Object[] columns = {"Description", "Status", "Ticket number", "Name"};
+    private Object[] columns = { "Description", "Status", "Ticket number", "Name" };
     private Object[] row = new Object[4];
     private JButton newBtn = new JButton(" New ");
     DefaultTableModel model = new DefaultTableModel();
 
-    //load lists
+    // load lists
     private List<Person> personList = jdbc.loadPeopleToList();
     private List<Ticket> ticketList = jdbc.loadTicketsToList(personList);
 
@@ -33,23 +33,25 @@ public class EmployeeWindow {
         panel = new JPanel();
         setUP();
     }
-    //main setup for frame
+
+    // main setup for frame
     public void setUP() {
         frame.setBounds(400, 200, 700, 600);
         frame.setLayout(new BorderLayout());
-        panel.setPreferredSize(new Dimension(700,300));
+        panel.setPreferredSize(new Dimension(700, 300));
         panel.setLayout(new BorderLayout());
         setTable();
-        frame.add(scrollPane,BorderLayout.CENTER);
-        newBtn.setPreferredSize(new Dimension(50,20));
+        frame.add(scrollPane, BorderLayout.CENTER);
+        newBtn.setPreferredSize(new Dimension(50, 20));
         newBtn.addActionListener(e -> newTicket());
-        panel.add(newBtn,BorderLayout.SOUTH);
-        frame.add(panel,BorderLayout.SOUTH);
+        panel.add(newBtn, BorderLayout.SOUTH);
+        frame.add(panel, BorderLayout.SOUTH);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
-    //creates table and makes it un-editable
+
+    // creates table and makes it un-editable
     public void setTable() {
         table = new JTable() {
             private static final long serialVersionUID = 1L;
@@ -66,12 +68,13 @@ public class EmployeeWindow {
         table.setRowHeight(30);
         table.setPreferredScrollableViewportSize(new Dimension(650, 400));
         scrollPane = new JScrollPane(table);
-        //load data to table line 72
+        // load data to table line 72
         loadTable();
-        //function to add event listener for double click line 83
+        // function to add event listener for double click line 83
         addActionToTable();
     }
-    //load data to table
+
+    // load data to table
     public void loadTable() {
         for (int i = 0; i < ticketList.size; i++) {
             Ticket T = ticketList.getInstance(i);
@@ -87,28 +90,29 @@ public class EmployeeWindow {
         table.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                //check for double click
-                if(e.getClickCount()==2){
-                    JTable target = (JTable)e.getSource();
+                // check for double click
+                if (e.getClickCount() == 2) {
+                    JTable target = (JTable) e.getSource();
                     int row = target.getSelectedRow();
-                    int ticketNum = (int) table.getValueAt(row,2);
-                    String status = (String) table.getValueAt(row,1);
-                    if (status.equals("complete")){
+                    int ticketNum = (int) table.getValueAt(row, 2);
+                    String status = (String) table.getValueAt(row, 1);
+                    if (status.equals("complete")) {
                         JOptionPane.showMessageDialog(frame,
                                 "This ticket has been complete,\nUnable to edit, please create a new ticket.",
-                                "Warning",
-                                JOptionPane.WARNING_MESSAGE);}
-                    else {
-                    frame.dispose();
-                    //close frame and open ticket window with ticket
-                    new TicketWindow(ticketList.findByID(ticketNum));}
+                                "Warning", JOptionPane.WARNING_MESSAGE);
+                    } else {
+                        frame.dispose();
+                        // close frame and open ticket window with ticket
+                        new TicketWindow(ticketList.findByID(ticketNum));
+                    }
                 }
             }
         });
     }
-    //create new ticket
-    public void newTicket(){
+
+    // create new ticket
+    public void newTicket() {
         frame.dispose();
-        TicketWindow ticketWindow = new TicketWindow(1);
+        new TicketWindow(1);
     }
 }
--- class/windows/TicketWindow.java
+++ class/windows/TicketWindow.java
@@ -1,15 +1,11 @@
 package windows;
 
 import Server.myJDBC;
-import ongoing.List;
 import ongoing.Person;
 import ongoing.Ticket;
-import windows.EmployeeWindow;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
@@ -25,29 +21,30 @@ public class TicketWindow {
     private JTextField UserNameText;
     private JTextField UserPhoneText;
     private JTextField UserMailText;
-    private JLabel StatusLbl=new JLabel("Status ");
-    private JLabel DescriptionLbl=new JLabel("Description ");
-    private JLabel ticketNumLbl=new JLabel("Ticket number ");
-    private JLabel UserNameLbl=new JLabel("Name  ");
-    private JLabel UserPhoneLbl=new JLabel("Phone ");
-    private JLabel UserMailLbl=new JLabel("Mail ");
+    private JLabel StatusLbl = new JLabel("Status ");
+    private JLabel DescriptionLbl = new JLabel("Description ");
+    private JLabel ticketNumLbl = new JLabel("Ticket number ");
+    private JLabel UserNameLbl = new JLabel("Name  ");
+    private JLabel UserPhoneLbl = new JLabel("Phone ");
+    private JLabel UserMailLbl = new JLabel("Mail ");
     private JButton SubmitBtn = new JButton("Submit");
     private JButton UpdateBtn = new JButton("Update");
 
-// constructor for new ticket
-    public TicketWindow(int logedIn){
+    // constructor for new ticket
+    public TicketWindow(int logedIn) {
         frame = new JFrame();
         initTextFields();
         setUP();
         ticketNumText.setEditable(false);
         UpdateBtn.setEnabled(false);
         SubmitBtn.addActionListener(e -> {
-           addTicket();
+            addTicket();
         });
         this.logedIn = logedIn;
     }
-    //constructor for existing ticket
-    public TicketWindow(Ticket ticket){
+
+    // constructor for existing ticket
+    public TicketWindow(Ticket ticket) {
         frame = new JFrame();
         initTicket(ticket);
         setUP();
@@ -55,34 +52,37 @@ public class TicketWindow {
         UpdateBtn.addActionListener(e -> {
             updateTicket(ticket);
         });
-        this.logedIn=1;
+        this.logedIn = 1;
     }
-    //main setup for frame
-    public void setUP(){
-        frame.setBounds(400,200,700,600);
+
+    // main setup for frame
+    public void setUP() {
+        frame.setBounds(400, 200, 700, 600);
         frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
-        frame.setLayout(new GridLayout(7,2,10,5));
+        frame.setLayout(new GridLayout(7, 2, 10, 5));
         addFields();
-        //add listener on window close to enter employee window
-        frame.addWindowListener(new WindowAdapter()
-        {@Override
-            public void windowClosing(WindowEvent e){
-            try {
-                if (logedIn==1)
-                    new EmployeeWindow();
-                else
-                    new MainWindow();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
+        // add listener on window close to enter employee window
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                try {
+                    if (logedIn == 1)
+                        new EmployeeWindow();
+                    else
+                        new MainWindow();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
             }
-        }});
+        });
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-    //add labels and text fields
-    public void addFields(){
+
+    // add labels and text fields
+    public void addFields() {
         frame.add(StatusLbl);
         frame.add(StatusText);
         frame.add(DescriptionLbl);
@@ -98,37 +98,40 @@ public class TicketWindow {
         frame.add(SubmitBtn);
         frame.add(UpdateBtn);
     }
-    //empty text for new ticket
-    public void initTextFields(){
-        StatusText=new JTextField();
-        DescriptionText=new JTextField();
-        ticketNumText=new JTextField();
-        UserNameText=new JTextField();
-        UserPhoneText=new JTextField();
-        UserMailText=new JTextField();
+
+    // empty text for new ticket
+    public void initTextFields() {
+        StatusText = new JTextField();
+        DescriptionText = new JTextField();
+        ticketNumText = new JTextField();
+        UserNameText = new JTextField();
+        UserPhoneText = new JTextField();
+        UserMailText = new JTextField();
     }
-    //fill existing ticket
-    public void initTicket(Ticket T){
-        StatusText=new JTextField(T.getStatus());
-        DescriptionText=new JTextField(T.getDescription());
+
+    // fill existing ticket
+    public void initTicket(Ticket T) {
+        StatusText = new JTextField(T.getStatus());
+        DescriptionText = new JTextField(T.getDescription());
         DescriptionText.setEditable(false);
-        ticketNumText=new JTextField((Integer.toString(T.getTicketNum())));
+        ticketNumText = new JTextField((Integer.toString(T.getTicketNum())));
         ticketNumText.setEditable(false);
-        UserNameText=new JTextField(T.getName());
+        UserNameText = new JTextField(T.getName());
         UserNameText.setEditable(false);
-        UserPhoneText=new JTextField(T.getPhone());
+        UserPhoneText = new JTextField(T.getPhone());
         UserPhoneText.setEditable(false);
-        UserMailText=new JTextField(T.getMail());
+        UserMailText = new JTextField(T.getMail());
         UserMailText.setEditable(false);
     }
-    public void addTicket(){
+
+    public void addTicket() {
         String description = DescriptionText.getText();
         String status = StatusText.getText();
         String name = UserNameText.getText();
         String phone = UserPhoneText.getText();
         String mail = UserMailText.getText();
-        Person p = new Person(name,phone,mail);
-        Ticket t = new Ticket(status,description,p);
+        Person p = new Person(name, phone, mail);
+        Ticket t = new Ticket(status, description, p);
         try {
             myJDBC sql = new myJDBC();
             sql.newPerson(p);
@@ -141,10 +144,11 @@ public class TicketWindow {
             ex.printStackTrace();
         }
     }
-    public void updateTicket(Ticket ticket){
+
+    public void updateTicket(Ticket ticket) {
         try {
             myJDBC sql = new myJDBC();
-            sql.updateTicketStatus(StatusText.getText(),ticket.getTicketNum());
+            sql.updateTicketStatus(StatusText.getText(), ticket.getTicketNum());
             frame.dispose();
             new EmployeeWindow();
         } catch (IOException ex) {
@@ -154,6 +158,5 @@ public class TicketWindow {
         }
 
     }
-
 
 }
