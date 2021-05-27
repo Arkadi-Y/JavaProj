d190d6dfe76198626d27f7e36e8f23702f75c3dc
arkadi y <arkadiyordan@gmail.com>
2021-05-27 16:13:48 +0300

    update

--- class/Server/myJDBC.java
+++ class/Server/myJDBC.java
@@ -2,201 +2,192 @@ package Server;
 
 import java.io.*;
 import java.sql.*;
-import Interfaces.*;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 import ongoing.*;
-
-public class myJDBC implements myJDBCInt {
-    private String sqlUrl;
-    private String user;
-    private String password;
-
+//main connection class to database
+public class myJDBC {
+private String sqlUrl;
+private String user;
+private String password;
+// constructor calls getProperty class to set up connectrion.
+    //connection will be created at every method
     public myJDBC() throws IOException, SQLException {
         getPropertyValues getClass = new getPropertyValues();
-        String[] values = getClass.getPropValues();
+        String[]values = getClass.getPropValues();
         sqlUrl = values[0];
-        user = values[1];
+        user = values [1];
         password = values[2];
-
     }
-
-    // connection method
+    //connection method
     public void connect() throws SQLException {
-        try {
-            Connection connection = DriverManager.getConnection(sqlUrl, user, password);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+       try {
+           Connection connection = DriverManager.getConnection(sqlUrl,user,password);
+       }catch (Exception e){
+           e.printStackTrace();
+       }
     }
-
-    // un-used;
+    //un-used;
     public void getTickets(int ID) throws SQLException {
-        try {
-            Connection connection = DriverManager.getConnection(sqlUrl, user, password);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from tickets where idPerson=" + ID);
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("description"));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        try{
+        Connection connection = DriverManager.getConnection(sqlUrl,user,password);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from tickets where idPerson="+ID);
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("description"));
         }
-
+    }catch (Exception e){
+        e.printStackTrace();
     }
 
-    public String getQuery(String column, String table, String where, String ID) {
+    }
+    // un-used
+    public String getQuery(String column, String table, String where,String ID){
         String output = "";
-        try {
-            Connection connection = DriverManager.getConnection(sqlUrl, user, password);
+        try{
+            Connection connection = DriverManager.getConnection(sqlUrl,user,password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement
-                    .executeQuery("select " + column + " from " + table + " where " + where + "=" + ID);
+            ResultSet resultSet = statement.executeQuery("select "+column+" from "+table+" where "+where+"="+ID);
             while (resultSet.next()) {
 
             }
             return output;
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return null;
         }
     }
-
-    // checks if user name and password are in the database
-    public boolean validateLogin(String username, String userpassword) {
-        try {
-            Connection connection = DriverManager.getConnection(sqlUrl, user, password);
+    //checks if  user name and password are in the database to validate login.
+    public boolean validateLogin(String username,String userpassword){
+        try{
+            Connection connection = DriverManager.getConnection(sqlUrl,user,password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from employeelogin WHERE UserName ='" + username
-                    + "' AND Password ='" + userpassword + "'");
+            ResultSet resultSet = statement.executeQuery("select * from employeelogin WHERE UserName ='"+username+"' AND Password ='"+userpassword+"'");
             while (resultSet.next())
                 return true;
             return false;
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return false;
         }
 
     }
-
-    // return id by name and phone
-    public int findPerson(String name, String phone) {
-        try {
-            Connection connection = DriverManager.getConnection(sqlUrl, user, password);
+    //return id by name and phone
+    public int findPerson(String name, String phone){
+        try{
+            Connection connection = DriverManager.getConnection(sqlUrl,user,password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement
-                    .executeQuery("select * from person WHERE name ='" + name + "' AND phone ='" + phone + "'");
-            while (resultSet.next()) {
-                // System.out.println(resultSet.getInt("idPerson"));
-                return resultSet.getInt("idPerson");
-            }
+            ResultSet resultSet = statement.executeQuery("select * from person WHERE name ='"+name+"' AND phone ='"+phone+"'");
+            while (resultSet.next()){
+                //System.out.println(resultSet.getInt("idPerson"));
+                return resultSet.getInt("idPerson");}
             return -1;
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return -2;
         }
     }
-
-    // gets table info -> creates list of tickets
-    public List<Ticket> loadTicketsToList(List<Person> personList) {
-        List<Ticket> list = new List<>();
-        try {
-            Connection connection = DriverManager.getConnection(sqlUrl, user, password);
+    //gets table info -> creates list of tickets
+    // must get person list because ticket must be connected to a person
+    public List<Ticket> loadTicketsToList(List<Person>personList){
+        List<Ticket>list=new List<>();
+        try{
+            Connection connection = DriverManager.getConnection(sqlUrl,user,password);
             Statement statement = connection.createStatement();
-            // get all tickets
+            //get all tickets
             ResultSet resultSet = statement.executeQuery("select * from tickets");
-            while (resultSet.next()) {
-                // get values
+            while (resultSet.next()){
+                //get values
                 int ticketNum = resultSet.getInt("ticketNum");
                 String description = resultSet.getString("description");
                 String status = resultSet.getString("status");
                 int personId = resultSet.getInt("idPerson");
-                // find person who is connected to ticket
+                //find person who is connected to ticket
                 Person person = personList.findByID(personId);
-                if (person != null) {
-                    Ticket ticket = new Ticket(ticketNum, status, description, person);
+                if (person!=null){
+                    Ticket ticket = new Ticket(ticketNum,status,description,person);
                     list.add(ticket);
-                } else {
+                                 }
+                else {
+                    // no possible existence of personless ticket so no error handler.
                     System.out.println("no person");
+                    }
                 }
-            }
             return list;
 
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
         }
         return null;
     }
-
-    public List<Person> loadPeopleToList() {
+    public List<Person> loadPeopleToList(){
         List<Person> people = new List<>();
-        try {
-            Connection connection = DriverManager.getConnection(sqlUrl, user, password);
+        try{
+            Connection connection = DriverManager.getConnection(sqlUrl,user,password);
             Statement statement = connection.createStatement();
-            // get all people table
+            //get all people table
             ResultSet resultSet = statement.executeQuery("select * from person");
-            while (resultSet.next()) {
-                // get values from table
+            while (resultSet.next()){
+                //get values from table
                 int id = resultSet.getInt("idPerson");
                 String name = resultSet.getString("name");
                 String phone = resultSet.getString("phone");
                 String mail = resultSet.getString("mail");
-                // create person and add to list
-                Person person = new Person(id, name, phone, mail);
+                //create person and add to list
+                Person person = new Person(id,name,phone, mail);
                 people.add(person);
             }
             return people;
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
             return null;
         }
-
     }
-
+    //as name suggests
     public void updateTicketStatus(String status, int ID) throws SQLException {
         try {
             Connection connection = DriverManager.getConnection(sqlUrl, user, password);
             Statement statement = connection.createStatement();
             statement.executeUpdate("UPDATE tickets SET  status ='" + status + "' WHERE ticketNum ='" + ID + "'");
-        } catch (Exception e) {
+        }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
-
-    public void insertTicket(Ticket t) {
+    //as name suggests
+    public void insertTicket(Ticket t){
         try {
             Connection connection = DriverManager.getConnection(sqlUrl, user, password);
             Statement statement = connection.createStatement();
-            statement.executeUpdate("INSERT INTO tickets VALUES (" + t.getTicketNum() + ",'" + t.getDescription()
-                    + "','" + t.getStatus() + "'," + t.getId() + ")");
-        } catch (Exception e) {
+            statement.executeUpdate("INSERT INTO tickets VALUES ("+t.getTicketNum()+",'"+t.getDescription()+"','"+t.getStatus()+"',"+t.getId()+")");
+        }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
-
-    public void newPerson(Person p) {
+    //new person using person class
+    public void newPerson(Person p){
         try {
             Connection connection = DriverManager.getConnection(sqlUrl, user, password);
             Statement statement = connection.createStatement();
-            if (findPerson(p.getName(), p.getPhone()) > -1)
+            // checks for existing person before creating one
+            if(findPerson(p.getName(),p.getPhone())>-1)
                 System.out.println("person in sql");
             else
-                statement.executeUpdate("INSERT INTO person VALUES (" + p.getId() + ",'" + p.getName() + "','"
-                        + p.getPhone() + "','" + p.getMail() + "')");
-        } catch (Exception e) {
+                statement.executeUpdate("INSERT INTO person VALUES ("+p.getId()+",'"+p.getName()+"','"+p.getPhone()+"','"+p.getMail()+"')");
+        }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
-
-    public void newPerson(int id, String name, String phone, String mail) {
+    //new person when creating one at Person class constructor
+    public void newPerson(int id,String name, String phone,String mail) {
         try {
             Connection connection = DriverManager.getConnection(sqlUrl, user, password);
             Statement statement = connection.createStatement();
             int Id = findPerson(name, phone);
-            if (Id > -1)
+            if ( Id> -1)
                 System.out.println("exists");
             else
-                statement.executeUpdate(
-                        "INSERT INTO person VALUES (" + id + ",'" + name + "','" + phone + "','" + mail + "')");
+                statement.executeUpdate("INSERT INTO person VALUES (" + id+ ",'" + name + "','" + phone + "','" + mail + "')");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
