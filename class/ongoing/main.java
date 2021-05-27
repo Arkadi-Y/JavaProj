ef118d344e83d2b11ecdeecc1d7c1476b3acab9f
arkadi y <arkadiyordan@gmail.com>
2021-05-27 20:04:31 +0300

    setLookAndFeel fix

--- class/ongoing/main.java
+++ class/ongoing/main.java
@@ -2,12 +2,21 @@ package ongoing;
 
 import windows.*;
 
+import javax.swing.*;
+
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException, ClassNotFoundException {
         try {
-            new MainWindow();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (ClassNotFoundException e) {
+            UIManager.setLookAndFeel(UIManager.getLookAndFeel());
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
         }
+        new MainWindow();
     }
 }
