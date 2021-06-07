02a82d1e3d7227fd668c3c5a9ceed43dc6a601bb
arkadi y <arkadiyordan@gmail.com>
2021-06-07 18:17:40 +0300

    MAIN

---
+++ class/Main.java
@@ -0,0 +1,18 @@
+import Server.myDATA;
+import windows.MainWindow;
+import javax.swing.*;
+
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (ClassNotFoundException | UnsupportedLookAndFeelException e) {
+            UIManager.setLookAndFeel(UIManager.getLookAndFeel());
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        myDATA data=new myDATA();
+        new MainWindow(data);
+    }
+}
